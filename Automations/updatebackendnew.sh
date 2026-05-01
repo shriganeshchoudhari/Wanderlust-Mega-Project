6350afb3fc04046b0f6550ff7b009a4b637f6a48
shriganeshchoudhari <ganesh@mail.com>
2026-05-01 21:00:07 +0530

    feat: add automation scripts to update backend and frontend environment configurations

--- Automations/updatebackendnew.sh
+++ Automations/updatebackendnew.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Set the Instance ID and path to the .env file
-INSTANCE_ID="i-030da7d31a1dbbffc"
-
-# Retrieve the public IP address of the specified EC2 instance
-ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+# Set local IP or localhost for local deployment
+ipv4_address="localhost"
 
 # Path to the .env file
 file_to_find="../backend/.env.docker"
