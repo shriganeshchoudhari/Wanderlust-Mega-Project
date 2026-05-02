095062e082ad34ab3a4fcc1df800f6b344c4fb2f
shriganeshchoudhari <ganesh@mail.com>
2026-05-02 20:10:37 +0530

    feat: add shell script to automate EKS cluster and nodegroup provisioning

--- terraform/setup_eks.sh
+++ terraform/setup_eks.sh
@@ -20,7 +20,7 @@ echo "=== Creating EKS Nodegroup (this takes another 5-10 minutes) ==="
 eksctl create nodegroup --cluster=wanderlust \
                         --region=us-east-2 \
                         --name=wanderlust \
-                        --node-type=t2.large \
+                        --node-type=m7i-flex.large \
                         --nodes=2 \
                         --nodes-min=2 \
                         --nodes-max=2 \
